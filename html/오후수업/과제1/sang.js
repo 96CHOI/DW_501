002ed3d51d18e47128438e39e13f4564e102f61b
96CHOI <cyd684@gmail.com>
2022-10-05 18:01:16 +0900

    10.05 오후 과제

---
+++ html/오후수업/과제1/sang.js
@@ -0,0 +1,9 @@
+
+function menu() { // 메뉴 클릭시
+    document.getElementById('allmenu').style.backgroundColor = "#ffffff";
+    document.getElementById('allmenu').style.color="#4e4e4e";
+    document.getElementById('todaymenu').style.backgroundColor = "#ffffff";
+    document.getElementById('todaymenu').style.color="#4e4e4e";
+    document.getElementById('bestmenu').style.backgroundColor = "#ffffff";
+    document.getElementById('bestmenu').style.color="#4e4e4e";
+}
