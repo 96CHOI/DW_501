e2aa28ef866cdab6aeac40576729696a070fe88d
96CHOI <cyd684@gmail.com>
2022-10-17 12:53:08 +0900

    10.17 오전 수업

--- html/오전수업/javascript/1013~1014/object.js
+++ html/오전수업/javascript/1013~1014/object.js
@@ -1,13 +1,13 @@
 
-function movie(title, dir, year, genre){ //생성자 함수
+function movie(title, dir, year, genre) { //생성자 함수
     this.movie_title = title;
     this.movie_dir = dir;
     this.movie_year = year;
     this.movie_genre = genre;
 };
-movie.prototype.out = function(){
+movie.prototype.out = function () {
     return this.movie_title + "  " + this.movie_dir + "  " +
-    this.movie_year + "  " + this.movie_genre;
+        this.movie_year + "  " + this.movie_genre;
 };
 var movie_list = new Array(); //movie 객체 저장될 배열
 
@@ -18,23 +18,23 @@ window.onload = function () {
             this.classList.add("active");
         });
         input[i].addEventListener("blur", function () {
-            if(this.value.length == ''){
-            this.classList.remove("active");
+            if (this.value.length == '') {
+                this.classList.remove("active");
             };
         });
     };
 };
 
-function enroll(){
+function enroll() {
     var val_temp = document.getElementsByClassName("movie");
 
     var data = new movie(val_temp[0].value, val_temp[1].value,
         val_temp[2].value, val_temp[3].value);
-    
+
     movie_list.push(data);
     print();
 }
-function print(){
+function print() {
     var li = document.createElement("li");
     li.innerText = movie_list[movie_list.length - 1].out();
 
